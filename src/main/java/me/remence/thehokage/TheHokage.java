fb4e6989885aa6c6acfc1584033b475c6e7db819
Jacob <jacob.isho14@gmail.com>
2022-09-11 17:32:28 -0400

    Added @everyone option to polls

--- src/main/java/me/remence/thehokage/TheHokage.java
+++ src/main/java/me/remence/thehokage/TheHokage.java
@@ -1,21 +1,69 @@
 package me.remence.thehokage;
 
-import me.remence.thehokage.commands.Ping;
+import me.remence.thehokage.commands.AboutMeCommand;
+import me.remence.thehokage.commands.EatMiloCommand;
+import me.remence.thehokage.commands.PollCommand;
+import me.remence.thehokage.listeners.JoinLeaveListener;
+import me.remence.thehokage.utils.Constant;
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
 import javax.security.auth.login.LoginException;
 
-public class TheHokage {
-    public static void main(String[] args) throws LoginException {
-        JDABuilder builder = JDABuilder.createDefault(Constants.token);
+public class TheHokage extends ListenerAdapter {
 
-        builder.setActivity(Activity.playing("Rust"));
-        builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
+    public static void main(String[] args) throws LoginException, InterruptedException {
+        JDA jda = JDABuilder.createDefault(Constant.TOKEN)
+                .enableIntents(GatewayIntent.GUILD_MEMBERS)
+                .addEventListeners(new AboutMeCommand()) // []?
+                .addEventListeners(new JoinLeaveListener())
+                .addEventListeners(new EatMiloCommand())
+                .addEventListeners(new PollCommand())
+                .setActivity(Activity.watching("Naruto"))
+                .setStatus(OnlineStatus.ONLINE)
+                .build().awaitReady();
 
-        builder.addEventListeners(new Listener());
+        Guild guild = jda.getGuildById("759195383242424340");
 
-        builder.build();
+        if (guild != null) {
+            guild.updateCommands().addCommands(
+                    Commands.slash("aboutme", "Learn about your account."),
+                    Commands.slash("eatmilo", "Munch on Milo."),
+                    Commands.slash("poll", "Send a poll for everyone to vote on!")
+                            .addOption(OptionType.STRING, "title", "Title the poll.", true)
+                            .addOption(OptionType.STRING, "description", "Describe what the poll is about.", true)
+                            .addOption(OptionType.CHANNEL, "channel", "Channel to send this poll to.", true)
+                            .addOption(OptionType.STRING, "opt-1", "Option for the poll.", true)
+                            .addOption(OptionType.STRING, "opt-2", "Option for the poll.", true)
+                            .addOption(OptionType.STRING, "opt-3", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-4", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-5", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-6", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-7", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-8", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-9", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-10", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-11", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-12", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-13", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-14", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-15", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-16", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-17", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-18", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-19", "Option for the poll.", false)
+                            .addOption(OptionType.STRING, "opt-20", "Option for the poll.", false)
+            ).queue();
+        }
     }
+
 }
+
+// Action? Queue it.
