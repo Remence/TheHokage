35d615c627b13f6961295d0b036caed02e1c711e
jacobisho <jacob.isho14@gmail.com>
2022-12-09 16:38:01 -0500

    Changed colour of the poll embed.

---
+++ src/main/java/me/remence/thehokage/utils/Constant.java
@@ -0,0 +1,6 @@
+package me.remence.thehokage.utils;
+
+public abstract class Constant {
+    public static String TOKEN = "ODQyOTE4ODI1OTMwMzkxNTcz.GYqPkm.K4f6X61a1bU26HX_L8ZOeTDhp6DsUr-7xQ4Elk";
+    public static String PREFIX = "!";
+}
