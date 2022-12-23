47eb0e20e39b43ed48b9c62926007113053f105f
jacobisho <jacob.isho14@gmail.com>
2022-12-23 18:20:35 -0500

    Fixed poll username bug.

--- src/main/java/me/remence/thehokage/utils/Truths.java
+++ src/main/java/me/remence/thehokage/utils/Truths.java
@@ -3,8 +3,8 @@ package me.remence.thehokage.utils;
 import java.util.Random;
 
 public class Truths {
-    public String[] truths = {"",
-    ""};
+    public String[] truths = {"What is the worst thing you've done?",
+    "What's your biggest fear?"};
 
     public String generateTruth() {
         Random rand = new Random();
